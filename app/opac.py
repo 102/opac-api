6d4a6c18c23ab7451c731e579ed2ad47e0039860
Roman Gusev <rgusev@hwdtech.com>
2016-03-18 15:00:47 +0600

    Add ability to change query length

--- app/opac.py
+++ app/opac.py
@@ -3,7 +3,8 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-a', '--author', required=True)
-args = parser.parse_args();
+parser.add_argument('-m', '--amount', help='Amount of books in result', default='10')
+args = parser.parse_args()
 
 opac = OPACWrapper()
-print(opac.get_book_list_by_author(args.author))
+print(opac.get_book_list_by_author(args.author, args.amount))
--- app/opacwrapper.py
+++ app/opacwrapper.py
@@ -2,12 +2,13 @@ import urllib.request
 import urllib.parse
 import re
 import xml.etree.ElementTree as ET
+import util.xmltodict
 
 
 class OPACWrapper(object):
     username = 'GUEST'
     password = 'GUESTE'
-    typeaccess = 'PayAccess'
+    type_access = 'PayAccess'
 
     OPAC_INIT_URL = 'http://opac.omsklib.ru/cgiopac/opacg/opac.exe'
     OPAC_DIRECT_URL = 'http://opac.omsklib.ru/cgiopac/opacg/direct.exe'
@@ -15,7 +16,7 @@ class OPACWrapper(object):
     session_id = '-1'
 
     def __init__(self):
-        params = urllib.parse.urlencode({'arg0': self.username, 'arg1': self.password, 'TypeAccess': self.typeaccess})
+        params = urllib.parse.urlencode({'arg0': self.username, 'arg1': self.password, 'TypeAccess': self.type_access})
         url = self.OPAC_INIT_URL + ('?%s' % params)
         with urllib.request.urlopen(url) as f:
             response = f.read().decode('utf-8').split('\r\n')
@@ -23,23 +24,20 @@ class OPACWrapper(object):
                 if not line.find('numsean') == -1:
                     self.session_id = re.findall(r'"([^"]*)"', line)[0]
 
-    def get_book_list_by_author(self, author_name):
+    def get_book_list_by_author(self, author_name, length='10'):
         data = urllib.parse.urlencode({'_errorXsl': '/opacg/html/common/xsl/error.xsl',
                                        '_wait:6M': '_xsl:/opacg/html/search/xsl/search_results.xsl',
                                        '_version': '2.5.0', '_service': 'STORAGE:opacfindd:FindView',
                                        'outformList[0]/outform': 'SHOTFORM', 'outformList[1]/outform': 'LINEORD',
-                                       'length': '15', 'query/body': '(AU {0})'.format(author_name),
+                                       'length': length, 'query/body': '(AU {0})'.format(author_name),
                                        'query/open': "{NC:<span class='red_text'>}", 'query/close': '{NC:</span>}',
                                        'userId': self.username, 'session': self.session_id, 'iddb': '2',
                                        'level[0]': 'Full', 'level[1]': 'Retro'})
         url = self.OPAC_DIRECT_URL
         with urllib.request.urlopen(url, data=data.encode('UTF-8')) as f:
             response = f.read().decode('utf-8')
-            books = []
             tree = ET.ElementTree(ET.fromstring(response))
+            root = tree.getroot()
+            books = map(lambda i: i['SHOTFORM']['content']['entry'], util.xmltodict.XmlListConfig(root)[0])
 
-            for element in tree.findall('.//entry'):
-                if element.text is not None and not element.text.lower().find(author_name.lower()) == -1:
-                    books.append(element.text)
-
-            return books
+            return list(books)
